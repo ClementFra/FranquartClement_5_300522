97670a824596621cbd778c03d7f4a913eea525dc
ClementFra <franquart.clemnt@gmail.com>
2022-07-03 21:33:09 +0200

    redirection of the order

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,14 +1,12 @@
 // Url of the order
 const urlSearchParams = new URLSearchParams(window.location.search);
 const elementOrderId = urlSearchParams.get("orderId");
-// number of the order 
-/*if (elementOrderId != null){
-    orderElement = document.getElementById("orderId");
-    windows.location = "../html/index.html"
+// number of the order
+// if the order = null the user was be redirected in the index 
+if (elementOrderId == null) {
+  window.location = "../html/index.html";
+  // the user get the number of the order
+} else {
+  orderElement = document.getElementById("orderId");
+  orderElement.innerHTML = `${elementOrderId}`;
 }
-*/
-orderElement = document.getElementById("orderId");
-orderElement.innerHTML = `${elementOrderId}`;
- 
-
-
